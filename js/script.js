66b4016b8d2cc20bf1f2174ef40585407366dd88
affan_tampubolon <affan.tampubolon@gmail.com>
2018-03-01 18:50:37 +0700

    merubah style

--- js/script.js
+++ js/script.js
@@ -1,87 +1,21 @@
-var TxtType = function(el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
-    this.loopNum = 0;
-    this.period = parseInt(period, 25) || 2000;
-    this.txt = '';
-    this.tick();
-    this.isDeleting = false;
-};
-
-TxtType.prototype.tick = function() {
-    var i = this.loopNum % this.toRotate.length;
-    var fullTxt = this.toRotate[i];
-
-    if (this.isDeleting) {
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
-
-    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-
-    var that = this;
-    var delta = 200 - Math.random() * 100;
-
-    if (this.isDeleting) { delta /= 2; }
-
-    if (!this.isDeleting && this.txt === fullTxt) {
-        delta = this.period;
-        this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
-        this.isDeleting = false;
-        this.loopNum++;
-        delta = 500;
-    }
-
-    setTimeout(function() {
-        that.tick();
-    }, delta);
-};
-
-window.onload = function() {
-    var elements = document.getElementsByClassName('typewrite');
-    for (var i=0; i<elements.length; i++) {
-        var toRotate = elements[i].getAttribute('data-type');
-        var period = elements[i].getAttribute('data-period');
-        if (toRotate) {
-          new TxtType(elements[i], JSON.parse(toRotate), period);
-      }
-  }
-        // INJECT CSS
-        var css = document.createElement("style");
-        css.type = "text/css";
-        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
-        document.body.appendChild(css);
-    };
-    $('#myTabs a').click(function (e) {
-      e.preventDefault()
-      $(this).tab('show')
-  })
-
-    $(document).ready(function() {
-      $('.progress .progress-bar').css("width",
-        function() {
-            return $(this).attr("aria-valuenow") + "%";
-        }
-        )
-  });
-    $('#header').load('header.html')
-    $('#footer').load('footer.html')
-    $(document).ready(function(){
-    //FANCYBOX
-    //https://github.com/fancyapps/fancyBox
-    $(".fancybox").fancybox({
-        openEffect: "none",
-        closeEffect: "none"
-    });
+$(document).ready(function(){
+	$('.parallax').parallax();
+	
 });
-    $(document).ready(function(){
-        $(document).on('click', 'a[href^="#"]',function (e) {
-            e.preventDefault();
+$(document).ready(function(){
+$(".button-collapse").sideNav();
+});
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
 
-            var target = this.hash;
-            var $target = $(target);
+	    var target = this.hash;
+	    var $target = $(target);
 
-        });
-    });
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+});
